d7072ff9b7f466e8e38ebdf5dc9d64fb3b49a03e
Carlos Vinicios Martins Rocha <vinicios_martins2011@hotmail.com>
2018-11-28 11:26:58 -0300

    Bases splits for train

--- python_codes/augumentation.py
+++ python_codes/augumentation.py
@@ -1,20 +1,30 @@
 from keras.preprocessing.image import ImageDataGenerator,img_to_array, load_img
-from sklearn.model_selection import train_test_split
 import numpy as np
 import random
-import os
 import shutil
+import os
 
 pastas = ["/test", "/train", "/valid"]
 
-load_path_melanomas = "base/melanomas"
-load_path_normais = "base/normais"
+load_path_melanomas = "../bases/PH2_original/melanomas"
+load_path_normais = "../bases/PH2_original/normais"
 
 img_melanomas = os.listdir(load_path_melanomas)
 img_normais = os.listdir(load_path_normais)
 
+base_size = 200
+base_size_m = 40
+base_size_n = 160
+train_split_size = 0.4
+test_split_size = 0.3
+valid_split_size = 0.3
+
+base = "../bases/" + str(int(train_split_size * 100)) + "_" + str(int(valid_split_size * 100)) + "_" + str(int(test_split_size * 100))
+
+os.mkdir(base) #cria a pasta para armazenar os splits
+
 for split in range(0, 5):
-	path = "split"+str(split+1)
+	path = base + "/split"+str(split+1)
 	os.mkdir(path)
 	#criando pastas
 	for pasta in pastas:
@@ -26,18 +36,21 @@ for split in range(0, 5):
 		os.mkdir(path_n)
 		random.shuffle(img_melanomas)
 		random.shuffle(img_normais)
-		if(pasta == '/test' or pasta == '/valid'):
-			num_m = 8
-			num_n = 32
+		if pasta == '/test':
+			num_m = base_size_m * test_split_size
+			num_n = base_size_n * test_split_size
+		elif pasta == '/valid':
+			num_m = base_size_m * valid_split_size
+			num_n = base_size_n * valid_split_size 
 		else:
-			num_m = 24
-			num_n = 96
+			num_m = base_size_m * train_split_size
+			num_n = base_size_n * train_split_size
 		
 		#melanomas
-		for i in range(0, num_m):
+		for i in range(0, int(num_m)):
 			shutil.copy(load_path_melanomas + "/" + img_melanomas[i], path_m)
 		#normais
-		for i in range(0, num_n):
+		for i in range(0, int(num_n)):
 			shutil.copy(load_path_normais + "/" + img_normais[i], path_n)
 
 datagen = ImageDataGenerator(
@@ -52,8 +65,8 @@ datagen = ImageDataGenerator(
 )
 
 for i in range(0, 5):
-	load_path = "split" + str(i+1)+"/train/melanomas"
-	save_path = "split" + str(i+1)+"/train/melanomas"
+	load_path = base + "/split" + str(i+1)+"/train/melanomas"
+	save_path = base + "/split" + str(i+1)+"/train/melanomas"
 	print("Salvando")
 	for filename1 in os.listdir(load_path):
 
